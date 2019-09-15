eb589a8d217644be333577a31f8c00bd25f87f80
Lucas Viana <vianadossantoslucas@gmail.com>
2019-09-15 20:26:31 -0300

    [ADD] Exercício 02 da lista 01

--- lista01/exec02/src/Lampada.rb
+++ lista01/exec02/src/Lampada.rb
@@ -1,5 +1,24 @@
 class Lampada
-    def initialize
-        @isOn = false
+    def initialize(limiteCliques)
+        @estado = false
+        @numeroCliques = 0
+        @limiteCliques = limiteCliques
+    end
+
+    def click        
+        @numeroCliques = @numeroCliques + 1
+
+        if @numeroCliques < @limiteCliques
+            @estado = !@estado
+            puts "A lâmpada está: #{@estado ? 'Acesa': 'Apagada'}";
+        else
+            @estado = false
+            puts "A lâmpada está: Queimada";
+        end
+    end
+
+    def estado
+        @estado
+        puts "A lâmpada está: #{@estado ? 'Acesa': 'Apagada'}";
     end
 end
